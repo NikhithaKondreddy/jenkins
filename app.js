b3f51f6ec7f9ba857e969c62728432e12e2dc62d
NikhithaKondreddy <NikhithaKondreddygari@nmit-solutions.com>
2026-07-14 15:44:10 +0530

    poll scm

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json());        
 
 app.get('/', (req, res) => {
-    res.send('Hello, World!');
+    res.send('jenkins poll scm test');
 });
 
 // Start the server
